a978c3b604cdd5623d85dd6ded7fafee7c74b7ed
mikita.bahamazau <ereticor@gmail.com>
2022-01-05 17:29:58 +0300

    feat: add interfaces & templates for users and apps

---
+++ src/constants/appList.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "app",
+    keywords: ["func", "func2"],
+    description: "some cool app",
+  },
+  {
+    name: "app2",
+    keywords: ["func", "func2"],
+    description: "some cool app",
+  },
+  {
+    name: "app3",
+    keywords: ["func", "func2"],
+    description: "some cool app",
+  },
+  {
+    name: "app4",
+    keywords: ["func", "func2"],
+    description: "some cool app",
+  },
+];
---
+++ src/constants/user.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "sam winchester",
+  avatar: undefined,
+  role: "admin",
+};
