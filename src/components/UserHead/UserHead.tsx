09b0fdf182af7b2a15aa3ca284adef0cbe8a2356
mikita.bahamazau <ereticor@gmail.com>
2022-01-06 10:51:03 +0300

    feat: add user profile opening in header

--- src/components/UserHead/UserHead.tsx
+++ src/components/UserHead/UserHead.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import UserProfile from "components/UserProfile";
+
 import IUser from "interfaces/User";
 
 import defaultAvatar from "assets/icons/userDefault.svg";
@@ -5,18 +9,27 @@ import defaultAvatar from "assets/icons/userDefault.svg";
 import "./UserHead.sass";
 
 const UserHead = ({ user }: { user: IUser }) => {
+  const [isShowedModal, setIsShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setIsShowModal((prevState) => !prevState);
+  };
+
   return (
-    <div className="user__profile">
-      <img
-        src={user.avatar || defaultAvatar}
-        alt="user logo"
-        className={`user__avatar ${user.avatar ? "" : "default"}`}
-      />
-      <div className="user__info">
-        <p className="user__name">{user.name}</p>
-        <p className="user__role">{user.role}</p>
+    <>
+      <div className="user__profile" onClick={toggleModal}>
+        <img
+          src={user.avatar || defaultAvatar}
+          alt="user logo"
+          className={`user__avatar ${user.avatar ? "" : "default"}`}
+        />
+        <div className="user__info">
+          <p className="user__name">{user.name}</p>
+          <p className="user__role">{user.role}</p>
+        </div>
       </div>
-    </div>
+      <UserProfile user={user} isOpened={isShowedModal} close={toggleModal} />
+    </>
   );
 };
 
