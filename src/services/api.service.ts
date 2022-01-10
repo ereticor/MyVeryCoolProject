7f6d5ffc133a3222bf3184e2c670ca949e0da01e
mikita.bahamazau <ereticor@gmail.com>
2022-01-10 14:09:39 +0300

    feat: add basic fetch wrapper

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,11 +1,43 @@
-// interface IHttpClient {
-//   _baseUrl: string;
-//   _headers: string;
+// interface IApiService {
+//   baseUrl?: string;
+//   headers?: Headers;
 // }
 
-// export default class HttpClient implements IHttpClient {
-//   constructor(options = {}) {
-//     this._baseUrl = options.baseURL || "";
-//     this._headers = options.headers || "";
-//   }
-// }
+export interface IRequest {
+  api: "customer" | "";
+  options: RequestInit;
+}
+
+export default class ApiService {
+  static basicOptions = {
+    headers: new Headers({
+      "Content-type": "application/json",
+      authorization: `Bearer ${"access token"}`,
+    }),
+  };
+
+  static baseUrl = "https://fuchs-rmt-api-dev.azurewebsites.net/api/";
+
+  static async request({ api, options }: IRequest) {
+    try {
+      const promise = await fetch(this.baseUrl + api, {
+        ...this.basicOptions,
+        ...options,
+      });
+      const response = await promise.json();
+      return response;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  static async get({ api, options }: IRequest) {
+    return this.request({ api, options: { ...options, method: "GET" } });
+  }
+  static async post({ api, options }: IRequest) {
+    return this.request({ api, options: { ...options, method: "POST" } });
+  }
+  static async delete({ api, options }: IRequest) {
+    return this.request({ api, options: { ...options, method: "DELETE" } });
+  }
+}
---
+++ src/services/customer.service.ts
@@ -0,0 +1,16 @@
+// import ApiService from "./api.service";
+
+// export default class CustomService extends ApiService {
+//   static async getAllCustomers() {
+//     super.get({ "customer", })
+//   }
+//   static async getPageNumber() {
+//     super.get({ "customer", })
+//   }
+//   static async getPage() {
+//     super.get({ "customer", })
+//   }
+//   static async getCustomer() {
+//     super.get({ "customer", })
+//   }
+// }
