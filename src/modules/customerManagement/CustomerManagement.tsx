3612ba4e44e8bb2690de06bd86c9d8e1416c545d
mikita.bahamazau <ereticor@gmail.com>
2022-01-11 15:00:59 +0300

    feat: ad customer management module page

--- src/modules/customerManagement/CustomerManagement.tsx
+++ src/modules/customerManagement/CustomerManagement.tsx
@@ -1,9 +1,29 @@
+import { IconButton } from "@material-ui/core";
+import { Add, Search } from "@material-ui/icons";
+
+import DataTable from "components/DataTable";
+
+// import user from "constants/user";
+
+import userList from "constants/userList";
+
+import "./CustomerManagement.scss";
+
 const CustomerManagement = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea modi dolor
-      nesciunt perferendis rerum adipisci. Dignissimos exercitationem nobis
-      soluta pariatur.
+    <div className="customers">
+      <div className="customers__head">
+        <h3 className="customers__title">customers</h3>
+        <div className="customers__controls">
+          <IconButton aria-label="plus" onClick={close}>
+            <Search />
+          </IconButton>
+          <IconButton aria-label="plus" onClick={close}>
+            <Add />
+          </IconButton>
+        </div>
+      </div>
+      <DataTable dataArr={userList} />
     </div>
   );
 };
