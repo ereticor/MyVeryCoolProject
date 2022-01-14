4f16a9a4918d3970d1555bffd33e2a846b8bac15
mikita.bahamazau <ereticor@gmail.com>
2022-01-14 16:48:44 +0300

    refactor: change customer headers interface

--- src/helpers/getDisplayedValue/definedHeaders/customerHeaders.ts
+++ src/helpers/getDisplayedValue/definedHeaders/customerHeaders.ts
@@ -1,8 +1,28 @@
 export default [
-  { text: "Name", prop: "name", type: "string" },
-  { text: "created At", prop: "createdAt", type: "dateTime" },
-  { text: "created By", prop: "createdByName", type: "string" },
-  { text: "updated At", prop: "lastModifiedAt", type: "dateTime" },
-  { text: "updated By", prop: "lastModifiedByName", type: "string" },
-  { text: "sap code", prop: "sapCode", type: "number" },
+  { isEditable: true, text: "Name", prop: "name", type: "string" },
+  {
+    isEditable: false,
+    text: "created At",
+    prop: "createdAt",
+    type: "dateTime",
+  },
+  {
+    isEditable: false,
+    text: "created By",
+    prop: "createdByName",
+    type: "string",
+  },
+  {
+    isEditable: false,
+    text: "updated At",
+    prop: "lastModifiedAt",
+    type: "dateTime",
+  },
+  {
+    isEditable: false,
+    text: "updated By",
+    prop: "lastModifiedByName",
+    type: "string",
+  },
+  { isEditable: false, text: "sap code", prop: "sapCode", type: "number" },
 ];
--- src/interfaces/tableHeader.ts
+++ src/interfaces/tableHeader.ts
@@ -2,4 +2,5 @@ export default interface ITableHeader {
   text: string;
   prop: string;
   type: string;
+  isEditable: boolean;
 }
