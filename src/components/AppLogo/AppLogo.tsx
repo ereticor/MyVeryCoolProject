83da4a3b438a369e40da24b867a5dea58cd21dce
mikita.bahamazau <ereticor@gmail.com>
2022-01-18 11:33:12 +0300

    feat: add app logo animation on hover

--- src/components/AppLogo/AppLogo.tsx
+++ src/components/AppLogo/AppLogo.tsx
@@ -1,10 +1,18 @@
 import logo from "assets/icons/appLogo.png";
 import { Link } from "react-router-dom";
 
+import "./AppLogo.scss";
+
 const AppLogo = () => {
   return (
-    <Link to="/">
-      <img src={logo} alt="company logo" />
+    <Link to="/" className="logo__wrapper">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span>
+        <img src={logo} alt="company logo" />
+      </span>
     </Link>
   );
 };
