5135ed705e115536a578986e6fbabff3075dcb1b
mikita.bahamazau <ereticor@gmail.com>
2022-01-20 14:50:49 +0300

    feat: connect customer reducer to store

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,5 +2,7 @@ import { connectRouter } from "connected-react-router";
 import { History } from "history";
 import { combineReducers } from "redux";
 
+import { reducer as customerState } from "store/reducers/customer";
+
 export default (history: History<unknown>) =>
-  combineReducers({ router: connectRouter(history) });
+  combineReducers({ router: connectRouter(history), customerState });
