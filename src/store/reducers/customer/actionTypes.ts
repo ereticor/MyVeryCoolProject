fb51b4d26839bc42b21fee84f58dcf16b1d44503
mikita.bahamazau <ereticor@gmail.com>
2022-01-20 14:48:47 +0300

    feat: add store action types

---
+++ src/store/reducers/customer/actionTypes.ts
@@ -0,0 +1,35 @@
+export const getAllCustomersTypes = {
+  startType: "GET_ALL_CUSTOMER_REQUEST",
+  successType: "GET_ALL_CUSTOMER_SUCCESS",
+  errorType: "GET_ALL_CUSTOMER_FAILURE",
+};
+
+export const getCustomersPageTypes = {
+  startType: "GET_CUSTOMERS_PAGE_REQUEST",
+  successType: "GET_CUSTOMERS_PAGE_SUCCESS",
+  errorType: "GET_CUSTOMERS_PAGE_FAILURE",
+};
+
+export const getCustomerTypes = {
+  startType: "GET_CUSTOMER_REQUEST",
+  successType: "GET_CUSTOMER_SUCCESS",
+  errorType: "GET_CUSTOMER_FAILURE",
+};
+
+export const changeCustomerTypes = {
+  startType: "PUT_CUSTOMER_REQUEST",
+  successType: "PUT_CUSTOMER_SUCCESS",
+  errorType: "PUT_CUSTOMER_FAILURE",
+};
+
+export const createCustomerTypes = {
+  startType: "POST_CUSTOMER_REQUEST",
+  successType: "POST_CUSTOMER_SUCCESS",
+  errorType: "POST_CUSTOMER_FAILURE",
+};
+
+export const deleteCustomerTypes = {
+  startType: "DELETE_CUSTOMER_REQUEST",
+  successType: "DELETE_CUSTOMER_SUCCESS",
+  errorType: "DELETE_CUSTOMER_FAILURE",
+};
